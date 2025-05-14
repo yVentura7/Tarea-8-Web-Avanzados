38e027e23f5af974ac312e8dad1bfd09d2c55f62
yVentura7 <yamil.venutra@tecsup.edu.pe>
2025-05-13 20:43:24 -0500

    Tercer Commit

--- database.js
+++ database.js
@@ -1,7 +1,5 @@
-import { MongoClient } from 'mongodb';
-import dotenv from 'dotenv';
-
-dotenv.config();
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
 
 const uri = process.env.MONGO_URI;
 const client = new MongoClient(uri);
@@ -9,12 +7,12 @@ const client = new MongoClient(uri);
 async function connectDB() {
   try {
     await client.connect();
-    console.log("✅ Conectado a MongoDB");
+    console.log("☑ Conectado a MongoDB local sin autenticación");
     return client.db();
   } catch (error) {
-    console.error("❌ Error al conectar a MongoDB:", error);
+    console.error("✗ Error al conectar a MongoDB:", error);
     process.exit(1);
   }
 }
 
-export { connectDB, client };
+module.exports = { connectDB, client };
